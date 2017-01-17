123f4066d7d099068a51ce914fe865467ee52a40
ouxuewen <ouxuewen@360.cn>
2017-01-17 20:46:34 +0800

    modify vuex data

--- src/components/ConfirmOrder.vue
+++ src/components/ConfirmOrder.vue
@@ -32,7 +32,7 @@
 			    <div class="order order_show_wrapper" style="">
 			      <div class="order-inner vertical-middle">
 			        <div class="order-detail">
-			          共<span class="order_total_number"></span>件商品
+			          共<span class="order_total_number">{{selectedData.selectedCount}}</span>件商品
 			        </div>
 			        <div class="price vertical-middle">
 			          <img src="~static/img/arrow.png" alt="" style="width:0.2rem">
@@ -59,7 +59,7 @@
 			      <div class="order-inner vertical-middle">
 			        <div class="price vertical-middle">
 			          <p>
-			            应付：￥<span class="order_total_price">495.00</span>
+			            应付：￥<span class="order_total_price">{{selectedData.selectedPrice}}</span>
 			          </p>
 			        </div>
 			      </div>
@@ -78,31 +78,7 @@
 			    </div>
 			  </div>
 			</div>
-			<!-- <a href="javascript:;" @click='addAddress'>
-				<ul v-if="CchooseAddr">
-					<li>
-						<span>手机: {{CchooseAddr.phone}}; 姓名：{{CchooseAddr.name}}</span>
-					</li>
-					<li>
-					<span>{{CchooseAddr.province|city}};{{CchooseAddr.municipality|city}};{{CchooseAddr.region|city}};{{CchooseAddr.address}}</span>
-					</li>
-				</ul>
-				<div v-else>
-						去添加地址
-				</div>
-			</a> -->
-			<!-- <a href="javascript:;" @click='chooseAddress'>选择地址</a> -->
 		</div>
-<!-- 		<ul>
-			<li v-for='(item, index) in makeOrderList'>
-				<span>颜色: {{item.properties.color}}</span>
-				<span>尺寸: {{item.properties.size}}</span>
-				<span>质量: {{item.properties.quality}}</span>
-				<span>X {{item.num}}</span>
-				<span>价钱: {{item.cost}} 元</span>
-			</li>
-		</ul>
-		<div><span>总价：{{total}}</span></div> -->
 		<div class="order-footer">
       <div @click='makePurchase' class="order-footer order-detail-footer">
         微信支付
@@ -133,23 +109,10 @@
 <script>
 import HeadBar from './HeadBar'
 import { mapGetters, mapActions } from 'vuex'
-// import { makeOrderList, makeSure, defaultAddress, chooseAddr } from '../vuex/getters'
 import { generate } from '../util/general'
-// import { makeOrder, changeMakeStatus, getAddress } from '../vuex/actions'
+
 export default {
-	// vuex: {
-	// 	getters: {
-	// 		makeOrderList,
-	// 		makeSure,
-	// 		defaultAddress,
-	// 		chooseAddr
-	// 	},
-	// 	actions: {
-	// 		makeOrder,
-	// 		changeMakeStatus,
-	// 		getAddress
-	// 	}
-	// },
+
 	components: {
 		HeadBar
 	},
@@ -162,7 +125,8 @@ export default {
 	computed: {
 		...mapGetters ({
         chooseAddr: 'chooseAddr',
-        defaultAddress: 'defaultAddress'
+        defaultAddress: 'defaultAddress',
+        selectedData: 'selectedData',
     }),
 		CchooseAddr () {
 			return this.chooseAddr || this.defaultAddress
