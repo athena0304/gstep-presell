bc019287c3e92f2941c406a381a76e14895b4558
ouxuewen <ouxuewen@360.cn>
2017-02-26 17:38:03 +0800

    modify css

--- src/App.vue
+++ src/App.vue
@@ -12,16 +12,3 @@ export default {
   components: {}
 }
 </script>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  line-height: 1;
-}
-body {
-  background: #1b1919;
-  font-family: "微软雅黑";
-  font-size: 0.36rem;
-}
-</style>
--- src/components/PresellRule.vue
+++ src/components/PresellRule.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
-	<div v-for="item in ruleData">
+	<head-bar>
+	  <h3 class="title">预售规则</h3>
+	</head-bar>
+	<div class="home-main-body" v-for="item in ruleData">
 		<div class="short_area area">
 			<div class="splitbar">{{item.title}}</div>
 		</div>
@@ -13,36 +16,16 @@
 			<img  :src="item.image" alt="" />
 		</div>
 	</div>
-	<!-- <div class="short_area area">
-		<div class="splitbar">预售规则简介</div>
-	</div>
-	<div class="context_wrapper simple_context_wrapper">
-		<div class="context_inner simple_contxt_inner">
-			dfgdfgdfgdfgdfg
-		</div>
-	</div>
-	<div class="short_area area">
-		<div class="splitbar">怎么保证商家质量</div>
-	</div>
-	<div class="context_wrapper simple_context_wrapper">
-		<div class="context_inner simple_contxt_inner">
-			搜狗拼音输入法官网下载,荣获多个国内软件大奖的搜狗拼音输入法是一款打字超准、词库超大、速度飞快、外观
-		</div>
-	</div>
-	<div class="short_area area">
-		<div class="splitbar">退款规则</div>
-	</div>
-	<div class="context_wrapper simple_context_wrapper">
-		<div class="context_inner simple_contxt_inner">
-			搜狗拼音输入法官网下载,荣获多个国内软件大奖的搜狗拼音输入法是一款打字超准、词库超大、速度飞快、外观
-		</div>
-	</div> -->
     <router-link to="/my-order"><button>>Go to Foo</button</router-link>
 </div>
 </template>
 <script>
 import shop from '../api/shop'
+import HeadBar from './HeadBar'
 export default {
+	components: {
+          HeadBar
+      },
   	data () {
   		return {
   			ruleData: ''
